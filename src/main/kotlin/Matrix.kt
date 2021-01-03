8fa1533fa97ed5fe2958e0ea40be1148fe04b050
Wieland Schöbl <wulkanat@gmail.com>
2021-01-03 14:40:23 +0100

    make full header auto generate

--- src/main/kotlin/Matrix.kt
+++ src/main/kotlin/Matrix.kt
@@ -1,3 +1,5 @@
+import java.time.LocalDateTime
+
 /**
  * Prints the header to the command line
  */
@@ -8,76 +10,104 @@ fun main() {
     for (y in 0..4) {
         for (x in y until (7 - y)) {
             positions[y][x] = pos
-            println("#define pos${x}_$y $pos")
             pos++
         }
     }
     for (y in 0..4) {
         for (x in y until (7 - y)) {
             positions[x][7 - y] = pos
-            println("#define pos${7 - y}_$x $pos")
             pos++
         }
     }
     for (y in 0..4) {
         for (x in y until (7 - y)) {
             positions[7 - y][7 - x] = pos
-            println("#define pos${7 - x}_${7 - y} $pos")
             pos++
         }
     }
     for (y in 0..4) {
         for (x in y until (7 - y)) {
             positions[7 - x][y] = pos
-            println("#define pos${y}_${7 - x} $pos")
             pos++
         }
     }
-    println()
-    printMatrix(positions)
-    println()
-    printMatrixQuadrants(positions)
+
+    val header = """
+//
+// Generated on ${LocalDateTime.now()}
+//
+
+#ifndef UNTITLED4_LIGHTNING_MATRIX_H
+#define UNTITLED4_LIGHTNING_MATRIX_H
+
+/**
+ * x86_64 64 bit bitwise rotate right
+ */
+static inline unsigned long long rorq(unsigned long long var, unsigned short amount) {
+    __asm__("rorq %%cl,%0" : "=r" (var) : "0" (var), "c" (amount));
+    return var;
 }
 
-fun printMatrix(matrix: Array<Array<Int>>) {
-    println("// to get positions at runtime")
-    println(
-        matrix.joinToString(prefix = "static unsigned short mat_positions[8][8] = {\n", separator = ",\n", postfix = "\n}") {
-            it.joinToString(separator = ", ", prefix = "    {", postfix = "}") { number -> number.toString().padStart(2, ' ') }
-        }
-    )
+/**
+ * x86_64 64 bit bitwise rotate left
+ */
+static inline unsigned long long rolq(unsigned long long var, unsigned short amount) {
+    __asm__("rolq %%cl,%0" : "=r" (var) : "0" (var), "c" (amount));
+    return var;
 }
 
-fun printMatrixQuadrants(matrix: Array<Array<Int>>) {
-    println("// Quadrants:")
-    println(
-        matrix.joinToString(separator = "\n") {
-            it.joinToString(separator = " ", prefix = "// ") { number ->
-                when (number / 16) {
-                    0 -> "\\/"
-                    1 -> "< "
-                    2 -> "/\\"
-                    3 -> " >"
-                    else -> " " // never
-                }
-            }
-        }
-    )
+${printMatrixQuadrants(positions)}
+
+${printMatrix(positions)}
+
+#endif //UNTITLED4_LIGHTNING_MATRIX_H
+    """
+
+    println(header)
 }
 
-fun printMatrixBitwiseOps(matrix: Array<Array<Int>>) {
-    println("// set bits")
-    println(
-        matrix.joinToString(separator = "\n") {
-            it.joinToString(separator = " ", prefix = "// ") { number ->
-                when (number / 16) {
-                    0 -> "\\/"
-                    1 -> "< "
-                    2 -> "/\\"
-                    3 -> " >"
-                    else -> " " // never
-                }
-            }
+fun printMatrix(matrix: Array<Array<Int>>): String {
+    return """
+// to get positions at runtime"
+${matrix.joinToString(
+    prefix = "static unsigned short mat_positions[8][8] = {\n",
+    separator = ",\n",
+    postfix = "\n}") {
+        it.joinToString(separator = ", ", prefix = "    {", postfix = "}") { number -> number.toString().padStart(2, ' ')}
+}}
+    """
+}
+
+fun printMatrixQuadrants(matrix: Array<Array<Int>>): String {
+    return """
+// Quadrants:
+${matrix.joinToString(separator = "\n") {
+    it.joinToString(separator = " ", prefix = "// ") { number ->
+        when (number / 16) {
+            0 -> "\\/"
+            1 -> "< "
+            2 -> "/\\"
+            3 -> " >"
+            else -> " " // never
         }
-    )
+    }
+}}
+    """
+}
+
+fun printMatrixBitwiseOps(matrix: Array<Array<Int>>): String {
+    return """
+// set bits
+${matrix.joinToString(separator = "\n") {
+    it.joinToString(separator = " ", prefix = "// ") { number ->
+        when (number / 16) {
+            0 -> "\\/"
+            1 -> "< "
+            2 -> "/\\"
+            3 -> " >"
+            else -> " " // never
+        }
+    }
+}}
+    """
 }
